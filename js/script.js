8e6a561a7a9a3ad790eef91fc8c3faf5171d2465
sourav <sourav0w@gmail.com>
2023-07-12 09:44:36 +0530

    add

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const d = new Date();
+let year = d.getFullYear();
+
+
+var typed = new Typed('#typaping', {
+    strings: ['Full Stack  Developer.', 'Flutter Developer.', 'WEB Developer.'],
+    typeSpeed: 90,
+    lifeLike: true,
+    waitUntilVisible: true,
+    loop: true
+});
