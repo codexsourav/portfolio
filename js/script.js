7f392555eed09755b6458a6c986d9d60ac397d3c
Ysourav <you@example.com>
2023-07-17 08:13:54 +0530

    add css

--- js/script.js
+++ js/script.js
@@ -9,3 +9,22 @@ var typed = new Typed('#typaping', {
     waitUntilVisible: true,
     loop: true
 });
+
+$(document).ready(function () {
+    $('#menubar-btn').click(function (e) { 
+        e.preventDefault();
+        $('#menu-box').css('display', 'flex');
+    });
+    $('#menubar-close').click(function (e) { 
+        e.preventDefault();
+        $('#menu-box').css('display', 'none');
+    });
+    
+    // $('.closemenu a').click(function (e) { 
+    //     e.preventDefault();
+    // setTimeout(() => {
+    //     $('#menu-box').css('display', 'none');
+    // }, 500);
+    // });
+    
+});
